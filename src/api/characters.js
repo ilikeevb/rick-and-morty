cb465a87b8544673421880ade8f9007f8b5d056f
ilikeevb <ilikeevb@yandex.ru>
2025-01-22 15:41:20 +0500

    fix: catch response error

--- src/api/characters.js
+++ src/api/characters.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const BASE_URL = "https://rickandmortyapi.com/api/character";
 
 export const fetchCharacters = async (params = {}) => {
-    const response = await axios.get(BASE_URL, { params });
-    return response.data;
+    try {
+        const response = await axios.get(BASE_URL, { params });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return { results: [], info: { pages: 1 } };
+    }
 };
 
 export const fetchCharacterById = async (id) => {
